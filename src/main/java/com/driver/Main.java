a19f440df2bb7e2b93476219757c23db0e972298
Akash Rawat <aki11.akash@gmail.com>
2023-03-14 12:58:54 +0530

    assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
 
+        int ans = p.product(10, 20);
+        int ans1 = p.product(10, 20, 30);
+        double ans2 = p.product(10.5, 19.5);
+
+        System.out.println(ans +" "+ ans1 + " "+ ans2);
+
+    }
+}
+    class Product{
+
+    public int product(int x, int y) {
+
+        int sum = x * y;
+
+        return sum;
+    }
+
+    public int product(int x, int y, int z) {
+
+        int sum = x * y * z;
+
+        return sum;
+    }
+
+    public double product(double x, double y) {
+
+        double sum = x * y;
+
+        return sum;
+    }
 }
